62fffbd7e7aed51d7cb3329f1fec29b754491426
Manticora <999manticora999@gmail.com>
2023-06-13 13:17:00 +0300

    feat: add all three arrows

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function clock() {
+    const hoursArrow = document.querySelector('.hours')
+    const minutesArrow = document.querySelector('.minutes')
+    const secondsArrow = document.querySelector('.seconds')
+};
+clock();
