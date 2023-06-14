1aebd6c41fddf451780ff542179e54555bd40c85
Manticora <999manticora999@gmail.com>
2023-06-14 09:56:37 +0300

    feat: add markup

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
-function clock() {
-    const hoursArrow = document.querySelector('.hours')
-    const minutesArrow = document.querySelector('.minutes')
-    const secondsArrow = document.querySelector('.seconds')
-    const deg = 6;
+// function clock() {
+//     const hoursArrow = document.querySelector('.hours')
+//     const minutesArrow = document.querySelector('.minutes')
+//     const secondsArrow = document.querySelector('.seconds')
+//     const deg = 6;
 
-    setInterval(() => {
-        const day = new Date()
+//     setInterval(() => {
+//         const day = new Date()
 
-        const hours = day.getHours() * 30
-        const minutes = day.getMinutes() * deg
-        const seconds = day.getSeconds() * deg
+//         const hours = day.getHours() * 30
+//         const minutes = day.getMinutes() * deg
+//         const seconds = day.getSeconds() * deg
 
-        hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
-        minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
-        secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+//         hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
+//         minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+//         secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
 
-    }, 0)
-};
-clock();
+//     }, 0)
+// };
+// clock();
+
+
+// analog 2
