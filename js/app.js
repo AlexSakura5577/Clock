aba26379a7e69ff489ef46ebb017cd233ed73356
Manticora <999manticora999@gmail.com>
2023-06-19 14:02:40 +0300

    feat: the clock does not run correctly

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+const hoursArrow = document.querySelector('.hours')
+const minutesArrow = document.querySelector('.minutes')
+const secondsArrow = document.querySelector('.seconds')
+
 // function clock() {
-//     const hoursArrow = document.querySelector('.hours')
-//     const minutesArrow = document.querySelector('.minutes')
-//     const secondsArrow = document.querySelector('.seconds')
 //     const deg = 6;
 
 //     setInterval(() => {
@@ -21,3 +22,19 @@
 
 
 // analog 2
+function clock() {
+
+    setInterval(() => {
+        const day = new Date()
+
+        const hours = day.getHours() * 30
+        const minutes = day.getMinutes() * deg
+        const seconds = day.getSeconds() * deg
+
+        hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
+        minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+        secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+
+    }, 0)
+};
+// clock();
