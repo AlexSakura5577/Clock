2a685caebaacf479b45c92abd0a2bd0bf289e784
Manticora <999manticora999@gmail.com>
2023-06-20 13:43:18 +0300

    feat: update css styles

--- js/app.js
+++ js/app.js
@@ -2,26 +2,6 @@ const hoursArrow = document.querySelector('.hours')
 const minutesArrow = document.querySelector('.minutes')
 const secondsArrow = document.querySelector('.seconds')
 
-// analog 1
-// function clock() {
-//     const deg = 6;
-
-//     setInterval(() => {
-//         const day = new Date()
-
-//         const hours = day.getHours() * 30
-//         const minutes = day.getMinutes() * deg
-//         const seconds = day.getSeconds() * deg
-
-//         hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
-//         minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
-//         secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
-
-//     }, 0)
-// };
-// clock();
-
-
 // analog 2
 function clock() {
 
